dba4cf2b792c43735cbc104686ded7bc2ef3a8c3
NovanArdian <novanardian@smkwikrama.sch.id>
2024-12-09 09:08:10 +0700

    perubahan

--- database/migrations/2024_09_21_154305_create_products_table.php
+++ database/migrations/2024_09_21_154305_create_products_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('price');
+            $table->decimal('price', 10, 2); // Decimal untuk menyimpan angka dengan desimal
             $table->string('product_code');
             $table->text('description');
             $table->timestamps();
